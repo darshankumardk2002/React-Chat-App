8f97d7b40f33249c9fcba812717859e59f4f7808
darshankumardk2002 <147990169+darshankumardk2002@users.noreply.github.com>
2024-08-10 21:15:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-
-import React, { useState, useRef } from 'react';
-import './App.css';
-import { Auth } from './components/Auth';
-
-// component imports
-import { Chat } from './components/Chat';
-
-// cookie imports
+import React, { useState, useRef } from "react";
+import { Chat } from "./components/Chat";
+import { Auth } from "./components/Auth";
 import Cookies from "universal-cookie";
-import { signOut } from 'firebase/auth';
-import { auth } from './firebase-config';
+import "./App.css";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config.js";
+
+const cookies = new Cookies();
 
 function App() {
-  const cookies = new Cookies();
-  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null);
-  const roomInputRef = useRef(null);
 
+  const roomInputRef = useRef(null);
   const signUserOut = async () => {
     await signOut(auth);
     cookies.remove("auth-token");
     setIsAuth(false);
-    setRoom(null);
-  }
-
+    room(null);
+  };
+  
   if (!isAuth) {
     return (
       <div>
-        <Auth setIsAuth={setIsAuth}/>
+        <Auth setIsAuth={setIsAuth} />
       </div>
     );
   }
@@ -35,19 +31,23 @@ function App() {
   return (
     <>
       {room ? (
-        <Chat room={room}/>
-      ) : (
-          <div className="room">
-            <label>Enter Room Name:</label>
-            <input ref={roomInputRef}/>
-            <button onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
-          </div>
-        )}
+        <Chat room={room} />
+        ) : (
+        <div className="room">
+          <label className="lab"> Type room name </label>
+          <input ref={roomInputRef} />
+          <button
+            onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )}
       <div className="sign-out">
         <button onClick={signUserOut}>Sign Out</button>
+
       </div>
     </>
-  )
+  );
 }
 
 export default App;
