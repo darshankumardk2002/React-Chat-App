96828ec79474b9081429d31f4a44408a52e4af11
darshankumardk2002 <147990169+darshankumardk2002@users.noreply.github.com>
2024-08-10 21:15:46 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,25 +1,22 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
-
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGEie-v6kr6knSkLE6RAwO_S5WdZBuWic",
-  authDomain: "react-chatapp-6cd48.firebaseapp.com",
-  projectId: "react-chatapp-6cd48",
-  storageBucket: "react-chatapp-6cd48.appspot.com",
-  messagingSenderId: "712064395803",
-  appId: "1:712064395803:web:4ad6d44fa16384a2e226be"
+  apiKey: "AIzaSyAtB6ZLJ2FCCeCJXRqCJv3AUUtKViJx6i0",
+  authDomain: "chatapp-343d0.firebaseapp.com",
+  projectId: "chatapp-343d0",
+  storageBucket: "chatapp-343d0.appspot.com",
+  messagingSenderId: "889457695986",
+  appId: "1:889457695986:web:ec1c45ac2f1cdb829c046b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
